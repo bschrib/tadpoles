8ae7a2e2681de2a768f40eb075660aa161bb70f0
Brandon Schreiber <2302045+bschrib@users.noreply.github.com>
2019-08-02 17:11:32 -0400

    Pull in fixes from hongshunyang, remove brighthorizons changes, add BenV Google 2FA changes, adjust logic for filename creation to be dynamic, use hongshunyang exiftool fix for all filetypes

--- py/app.py
+++ py/app.py
@@ -8,6 +8,7 @@ import pickle
 import logging
 import logging.config
 import imghdr
+import datetime
 
 from random import randrange
 from getpass import getpass
@@ -65,10 +66,8 @@ class Client:
     COOKIE_FILE = "state/cookies.pkl"
     ROOT_URL = "http://www.tadpoles.com/"
     HOME_URL = "https://www.tadpoles.com/parents"
-    BH_URL = "https://familyinfocenter.brighthorizons.com/Account/Login"
-    LOGIN_URL = "https://familyinfocenter.brighthorizons.com/MyBrightDay/Redirect"
     MIN_SLEEP = 1
-    MAX_SLEEP = 3
+    MAX_SLEEP = 6
 
     def __init__(self):
         self.init_logging()
@@ -97,6 +96,7 @@ class Client:
         self.vdisplay.stop()
 
     def sleep(self, minsleep=None, maxsleep=None):
+        self.info("Executing sleep")
         _min = minsleep or self.MIN_SLEEP
         _max = maxsleep or self.MAX_SLEEP
         duration = randrange(_min * 100, _max * 100) / 100.0
@@ -104,28 +104,30 @@ class Client:
         time.sleep(duration)
 
     def navigate_url(self, url):
+        self.info("Executing navigate_url")
         self.info("Navigating to %r", url)
         self.br.get(url)
 
     def load_cookies(self):
-        self.info("Loading cookies.")
+        self.info("Executing load_cookies")
         if not isdir('state'):
             os.mkdir('state')
         with open(self.COOKIE_FILE, "rb") as f:
             self.cookies = pickle.load(f)
 
     def dump_cookies(self):
-        self.info("Dumping cookies.")
+        self.info("Executing dump_cookies")
         with open(self.COOKIE_FILE,"wb") as f:
             pickle.dump(self.br.get_cookies(), f)
 
     def add_cookies_to_browser(self):
-        self.info("Adding the cookies to the browser.")
+        self.info("Executing add_cookies_to_browser")
         for cookie in self.cookies:
-            #if self.br.current_url.strip('/').endswith(cookie['domain']):
-            self.br.add_cookie(cookie)
+            if self.br.current_url.strip('/').endswith(cookie['domain']):
+                self.br.add_cookie(cookie)
 
     def requestify_cookies(self):
+        self.info("Executing requestify_cookies")
         # Cookies in the form reqeusts expects.
         self.info("Transforming the cookies for requests lib.")
         self.req_cookies = {}
@@ -134,57 +136,57 @@ class Client:
 
     def switch_windows(self):
         '''Switch to the other window.'''
-        self.info("Switching windows.")
+        self.info("Executing switch_windows")
         all_windows = set(self.br.window_handles)
+        self.info("Displaying all_windows")
         self.info(all_windows)
         current_window = set([self.br.current_window_handle])
+        self.info("Displaying current_window")
         self.info(current_window)
         other_window = (all_windows - current_window).pop()
+        self.info("Displaying other_window")
+        self.info(other_window)
+        self.info("Performing switch to other_window")
         self.br.switch_to.window(other_window)
 
     def do_login(self):
         # Navigate to login page.
-        self.info("Navigating to login page.")
-        self.navigate_url(self.BH_URL)
-
-        # self.br.find_element_by_id("login-button").click()
-        # self.br.find_element_by_class_name("tp-block-half").click()
-        # self.br.find_element_by_class_name("other-login-button").click()
-
+        self.info("Executing do_login")
+        self.br.find_element_by_id("login-button").click()
+        self.br.find_element_by_class_name("tp-block-half").click()
+        self.br.find_element_by_class_name("other-login-button").click()
+        main_window = self.br.current_window_handle
+        self.info("Displaying main_window")
+        self.info(main_window)
+        self.main_window = self.br.current_window_handle
+        self.info("Current main_window is")
         self.info(self.br.current_url)
 
         # Enter email.
         self.info("  Sending username.")
-        email = self.br.find_element_by_id("UserName")
+        email = self.br.find_element_by_css_selector(".controls input[type='text']")
         email.send_keys(input("Enter email: "))
 
         # Enter password.
         self.info("  Sending password.")
-        passwd = self.br.find_element_by_id("Password")
+        passwd = self.br.find_element_by_css_selector(".controls input[type='password']")
         passwd.send_keys(input("Enter password: "))
 
         # Click "submit".
         self.info("Sleeping 2 seconds.")
         self.sleep(minsleep=2)
         self.info("Clicking 'sumbit' button.")
-        self.br.find_element_by_css_selector("input[type=\"submit\"]").click()
-        self.sleep(minsleep=2)
-        self.info("New url")
-        self.info(self.br.current_url)
-        self.navigate_url(self.LOGIN_URL)
-        self.sleep(minsleep=2)
-        self.info("New url")
-        self.info(self.br.current_url)
-        self.navigate_url(self.HOME_URL)
+        self.br.find_element_by_css_selector(".tp-left-contents .btn-primary").click()
         self.sleep(minsleep=2)
         self.info("New url")
         self.info(self.br.current_url)
 
     def do_google_login(self):
         # Navigate to login page.
-        self.info("Navigating to login page.")
+        self.info("Executing do_google_login")
         self.br.find_element_by_id("login-button").click()
         self.br.find_element_by_class_name("tp-block-half").click()
+        self.main_window = self.br.current_window_handle
 
         for element in self.br.find_elements_by_tag_name("img"):
             if "btn-google.png" in element.get_attribute("src"):
@@ -205,60 +207,54 @@ class Client:
         # Enter email.
         email = self.br.find_element_by_id("identifierId")
         email.send_keys(input("Enter email: "))
-        email.submit()
+        #email.submit()
         self.br.find_element_by_id("identifierNext").click()
 
-        self.info("Sleeping 2 seconds.")
-        self.sleep(minsleep=2)
+        self.info("Sleeping 5 seconds.")
+        self.sleep(minsleep=5)
 
         # Enter password.
-        #passwd = self.br.find_element_by_id("password")
-        #passwd.send_keys(getpass("Enter password:"))
-        #passwd.submit()
-
-        password = self.br.find_element_by_name("password")
+        password = self.br.find_element_by_css_selector("input[type=password]")
         password.send_keys(getpass("Enter password:"))
-        password.submit()
+        #password.submit()
         self.br.find_element_by_id("passwordNext").click()
 
         self.info("Sleeping 2 seconds.")
         self.sleep(minsleep=2)
 
-        # Enter 2FA pin.
-        pin = self.br.find_element_by_id("totpPin")
-        pin.send_keys(getpass("Enter google verification code: "))
-        pin.submit()
-        self.br.find_element_by_id("totpNext").click()
+        # 2FA can hopefully be handled by push notification on modern phones/apps
+        self.info("Sleeping 5 seconds.")
+        self.sleep(minsleep=5)
 
-        self.info("Sleeping 2 seconds.")
-        self.sleep(minsleep=2)
-
-        # Click "approve".
-        #self.info("Sleeping 2 seconds.")
-        #self.sleep(minsleep=2)
-        #self.info("Clicking 'approve' button.")
-        #self.br.find_element_by_id("submit_approve_access").click()
-
-        # Switch back to tadpoles.
-        self.switch_windows()
+        # Switch back to tadpoles
+        self.info("Switching back to main_window")
+        self.br.switch_to.window(self.main_window)
 
     def iter_monthyear(self):
         '''Yields pairs of xpaths for each year/month tile on the
         right hand side of the user's home page.
         '''
-        month_xpath_tmpl = '//*[@id="app"]/div[4]/div[1]/ul/li[%d]/div/div/div/div/span[%d]'
+        self.info("Executing iter_monthyear")
+        month_xpath_tmpl = '//*[@id="app"]/div[3]/div[1]/ul/li[%d]/div/div/div/div/span[%d]'
         month_index = 1
+        #self.info("The month_xpath_tmpl is {}".format(month_xpath_tmpl))
+        #self.info("The month_index is {}".format(month_index))
         while True:
             month_xpath = month_xpath_tmpl % (month_index, 1)
             year_xpath = month_xpath_tmpl % (month_index, 2)
+            #self.info("The month_xpath is {}".format(month_xpath))
+            #self.info("The year_xpath is {}".format(year_xpath))
 
             # Go home if not there already.
             if self.br.current_url != self.HOME_URL:
+                self.info("Navigating to {} for iter_monthyear function".format(self.HOME_URL))
                 self.navigate_url(self.HOME_URL)
             try:
                 # Find the next month and year elements.
                 month = self.br.find_element_by_xpath(month_xpath)
+                self.info("The month is {}".format(month))
                 year = self.br.find_element_by_xpath(year_xpath)
+                self.info("The year is {}".format(year))
             except NoSuchElementException:
                 # We reached the end of months on the profile page.
                 self.warning("No months left to scrape. Stopping.")
@@ -274,6 +270,7 @@ class Client:
         '''Find all the image urls on the current page.
         '''
         # For each month on the dashboard...
+        self.info("Executing iter_urls")
         for month, year in self.iter_monthyear():
             # Navigate to the next month.
             month.click()
@@ -298,6 +295,7 @@ class Client:
         '''
 
         # Make the local filename.
+        self.info("Executing save_image")
         _, key = url.split("key=")
         filename_parts = ['img', self.year.text, self.month.text, '%s']
         filename_base = abspath(join(*filename_parts) % key)
@@ -310,6 +308,12 @@ class Client:
         elif isfile(filename_base + '.png'):
             self.debug("Already downloaded: %s.png" % filename_base)
             return
+        elif isfile(filename_base + '.jpeg'):
+            self.debug("Already downloaded: %s.jpeg" % filename_base)
+            return
+        elif isfile(filename_base + '.mp4'):
+            self.debug("Already downloaded: %s.mp4" % filename_base)
+            return
         else:
             self.info("Saving: %s" % filename)
             self.sleep()
@@ -318,9 +322,9 @@ class Client:
         dr = dirname(filename)
         if not isdir(dr):
             os.makedirs(dr)
+            info.self("Made directory {}".format(dr))
 
         # Download it with requests.
-
         resp = requests.get(url, cookies=self.req_cookies, stream=True)
         if resp.status_code == 200:
             with open(filename, 'wb') as f:
@@ -335,34 +339,43 @@ class Client:
         yearmonth = self.year.text + ':' + months[self.month.text] + ':' + day + ' 12:00:00'
         ## check if the file is actually a png
         imgtype = imghdr.what(filename)
-        if imghdr.what(filename) == 'png':
-            self.info("  File is a png - renaming")
-            os.rename(filename, filename_base + '.png')
-            filename = filename_base + '.png'
-            command = 'exiftool -overwrite_original "-PNG:CreationTime=' + yearmonth + '" "' + filename + '"'
-            self.info("  Adding png exif: %s" % command)
-            os.system(command)
-
-            command = 'exiftool -overwrite_original "-AllDates=' + yearmonth + '" "' + filename + '"'
-            self.info("  Adding exif: %s" % command)
-            os.system(command)
-        else:
-            ## video 
-            self.info("  File is a video - renaming")
-            os.rename(filename, filename_base + '.mp4')
-            #filename = filename_base + '.mp4'
-            #command = 'exiftool -overwrite_original "-PNG:CreationTime=' + yearmonth + '" "' + filename + '"'
-            #self.info("  Adding png exif: %s" % command)
-            #os.system(command)
-
-        #command = 'exiftool -overwrite_original "-AllDates=' + yearmonth + '" "' + filename + '"'
-        #self.info("  Adding exif: %s" % command)
-        #os.system(command)
+        timeNow = datetime.datetime.now()
+        uniqueFileName = timeNow.strftime("%H%M%S%f")
+        newFileName = os.path.abspath(os.path.join(filename_base, os.pardir)) + '/' + yearmonth.replace(':','').replace(' 120000','') + '_tadpoles_' + uniqueFileName
+        # Get away from cases for each filetype and just build it for any type
+        self.info("  File is a {} - renaming".format(imgtype))
+        os.rename(filename, newFileName + '.' + imgtype)
+        filename = newFileName + '.' + imgtype
+        command = 'exiftool -overwrite_original "-AllDates=' + yearmonth + '" "' + filename + '"'
+        self.info("  Adding exif: %s" % command)
+        os.system(command)
+        #if imgtype == 'png':
+        #    self.info("  File is a png - renaming")
+        #    os.rename(filename, newFileName + '.png')
+        #    filename = newFileName + '.png'
+        #    command = 'exiftool -overwrite_original "-AllDates=' + yearmonth + '" "' + filename + '"'
+        #    self.info("  Adding exif: %s" % command)
+        #    os.system(command)
+        #elif imgtype == 'jpeg':
+        #    self.info("  File is a jpeg - renaming")
+        #    os.rename(filename, newFileName + '.jpeg')
+        #    filename = newFileName + '.jpeg'
+        #    command = 'exiftool -overwrite_original "-AllDates=' + yearmonth + '" "' + filename + '"'
+        #    self.info("  Adding exif: %s" % command)
+        #    os.system(command)
+        #else:
+        #    self.info("  File is a video - renaming")
+        #    os.rename(filename, newFileName + '.mp4')
+        #    filename = newFileName + '.mp4'
+        #    command = 'exiftool -overwrite_original "-AllDates=' + yearmonth + '" "' + filename + '"'
+        #    self.info("  Adding exif: %s" % command)
+        #    os.system(command)
 
     def download_images(self):
         '''Login to tadpoles.com and download all user's images.
         '''
-        self.navigate_url(self.BH_URL)
+        self.info("Executing download_images")
+        self.navigate_url(self.ROOT_URL)
 
         try:
             self.load_cookies()
@@ -400,6 +413,7 @@ class Client:
                 self.exception(exc)
 
     def main(self):
+        self.info("Executing main")
         with self as client:
             try:
                 client.download_images()
